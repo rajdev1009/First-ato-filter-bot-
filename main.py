3a85928988e1714a16eeaa3cdcb7302dfa93ba72
rajdev1009 <mrdev4772@gmail.com>
2025-12-14 21:45:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,47 @@
+import os
 import asyncio
-from pyrogram import Client
-from config import API_ID, API_HASH, BOT_TOKEN
+from pyrogram import Client, idle
+from aiohttp import web
+from config import Config
 
-app = Client(
-    "auto-filter-bot",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN,
-    plugins=dict(root="plugins")
-)
+routes = web.RouteTableDef()
+@routes.get("/", allow_head=True)
+async def root_route_handler(request):
+    return web.json_response({"status": "running"})
+
+async def web_server():
+    web_app = web.Application(client_max_size=30000000)
+    web_app.add_routes(routes)
+    return web_app
+
+class Bot(Client):
+    def __init__(self):
+        super().__init__("ProBot", api_id=Config.API_ID, api_hash=Config.API_HASH, bot_token=Config.BOT_TOKEN, plugins=dict(root="plugins"))
+
+    async def start(self):
+        await super().start()
+        print("✅ Raj HD Movies Bot Started!")
+        # 🔥 Auto Connect System 🔥
+        try:
+            print("🔄 Connecting to DB Channel...")
+            await self.get_chat(Config.DB_CHANNEL)
+            print("✅ DB Channel Connected!")
+        except Exception as e:
+            print(f"❌ Failed to connect: {e}")
+
+    async def stop(self, *args):
+        await super().stop()
 
 async def main():
-    await app.start()
-    print("✅ Bot Started")
-    await asyncio.Event().wait()
+    bot = Bot()
+    app = web.AppRunner(await web_server())
+    await app.setup()
+    await web.TCPSite(app, "0.0.0.0", int(os.environ.get("PORT", 8080))).start()
+    await bot.start()
+    await idle()
+    await bot.stop()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    
