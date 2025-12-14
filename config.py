2081089f822f53397e9ac6d7a568fe634baa2449
rajdev1009 <mrdev4772@gmail.com>
2025-12-14 10:41:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,22 +5,22 @@ load_dotenv()
 
 class Config:
     # Get these from my.telegram.org
-    API_ID = int(os.environ.get("API_ID", "1234567"))
-    API_HASH = os.environ.get("API_HASH", "your_api_hash")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "your_bot_token")
+    API_ID = int(os.environ.get("API_ID", "0"))
+    API_HASH = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
     # Database
-    MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "your_mongodb_url")
+    MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "")
 
-    # Channels (Use -100xxxxxx IDs)
-    DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-100xxxx")) # Where files are
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-100xxxx")) # User logs
-    PREMIUM_LOG_CHANNEL = int(os.environ.get("PREMIUM_LOG_CHANNEL", "-100xxxx")) # Payment logs
+    # Channels (must start with -100)
+    DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "0")) # Files here
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "0")) # Logs here
+    PREMIUM_LOG_CHANNEL = int(os.environ.get("PREMIUM_LOG_CHANNEL", "0")) # Payment Proofs
 
     # Admin
-    ADMINS = [int(x) for x in os.environ.get("ADMINS", "12345678").split()]
+    ADMINS = [int(x) for x in os.environ.get("ADMINS", "").split()]
     CREATOR_NAME = "Raj HD Movies"
 
     # Shortener (Sotnar)
     SHORTENER_API = os.environ.get("SHORTENER_API", "")
-    SHORTENER_URL = os.environ.get("SHORTENER_URL", "") # e.g., "shortener.com"
+    SHORTENER_URL = os.environ.get("SHORTENER_URL", "") 
