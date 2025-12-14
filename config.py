16fc7ea4b558d86a56f0f68529f6a6885311399c
rajdev1009 <mrdev4772@gmail.com>
2025-12-14 21:08:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,23 +1,9 @@
 import os
-import ast
 
-# ───────────── TELEGRAM ─────────────
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-# ───────────── DATABASE ─────────────
 MONGO_URI = os.getenv("MONGO_URI")
-DB_NAME = os.getenv("DB_NAME", "AutoFilterBot")
 
-# ───────────── ADMINS ─────────────
-ADMINS = ast.literal_eval(
-    os.getenv("ADMINS", "[]")
-)
-
-# ───────────── OMDB / IMDB ─────────────
-OMDB_API_KEY = os.getenv("OMDB_API_KEY")
-
-# ───────────── BOT SETTINGS ─────────────
-RESULTS_PER_PAGE = int(os.getenv("RESULTS_PER_PAGE", 10))
-AUTO_DELETE_TIME = int(os.getenv("AUTO_DELETE_TIME", 600))
+RESULTS_PER_PAGE = 5
