79312f63d8daf9117bfde7b433739f81f4a6b40a
rajdev1009 <mrdev4772@gmail.com>
2025-12-14 20:50:19 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,13 +1,1 @@
-project/
-│
-├── main.py
-├── config.py
-├── database.py
-├── utils.py
-├── requirements.txt
-│
-└── plugins/
-    ├── __init__.py   ✅ (EMPTY / SIMPLE)
-    ├── admin.py
-    ├── commands.py
-    └── filter.py
+
