4298d5305dbee5d4b500bc6412e031f0bb7c2702
rajdev1009 <mrdev4772@gmail.com>
2025-12-14 12:42:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,19 +4,24 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
+    # Telegram API
     API_ID = int(os.environ.get("API_ID", "0"))
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    
+
+    # Database
     MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "")
 
-    # यहाँ int() बहुत जरूरी है
-    DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002795064458"))
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002746487551"))
-    PREMIUM_LOG_CHANNEL = int(os.environ.get("PREMIUM_LOG_CHANNEL", "-1002746487551"))
+    # Channels (Auto-Convert to Integer)
+    # अगर आपने गलती से "" लगा दिया है, तो यह उसे हटाकर नंबर बना देगा
+    DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "0"))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "0"))
+    PREMIUM_LOG_CHANNEL = int(os.environ.get("PREMIUM_LOG_CHANNEL", "0"))
 
-    ADMINS = [int(x) for x in os.environ.get("ADMINS", "").split()]
+    # Admin
+    ADMINS = [int(x) for x in os.environ.get("ADMINS", "0").split()]
     CREATOR_NAME = "Raj HD Movies"
 
+    # Shortener
     SHORTENER_API = os.environ.get("SHORTENER_API", "")
     SHORTENER_URL = os.environ.get("SHORTENER_URL", "") 
