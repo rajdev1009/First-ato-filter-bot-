88ebc756c333e02fb1b5dc76e830dc41034deee0
rajdev1009 <mrdev4772@gmail.com>
2025-12-14 16:23:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,22 +4,25 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    # Get these from my.telegram.org
     API_ID = int(os.environ.get("API_ID", "0"))
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-    # Database
     MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "")
 
-    # Channels (IDs must start with -100)
+    # Channel IDs (Auto Fix -100)
     DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "0"))
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "0"))
-    
-    # Links (Update these!)
-    UPDATE_CHANNEL_LINK = "https://t.me/+YZ7qQ1Ahx-M1MDdl" 
-    MOVIE_GROUP_LINK = "https://t.me/+mgQzW_pjxT1hODI1"
+    UPDATE_CHANNEL = int(os.environ.get("UPDATE_CHANNEL", "0")) 
 
-    # Admins
+    # Links
+    UPDATE_CHANNEL_LINK = "https://t.me/+YZ7qQ1Ahx-M1MDdl"
+    MOVIE_GROUP_LINK = "https://t.me/+mgQzW_pjxT1hODI1"
+    ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "@raj_dev_01") 
+
+    # Admin
     ADMINS = [int(x) for x in os.environ.get("ADMINS", "0").split()]
-    
+
+    # Shortener (Optional)
+    SHORTENER_API = os.environ.get("SHORTENER_API", "")
+    SHORTENER_URL = os.environ.get("SHORTENER_URL", "") 
