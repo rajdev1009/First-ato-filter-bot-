eb8bebc8b617f4e995dc9e9762784946ef52a516
rajdev1009 <mrdev4772@gmail.com>
2025-12-14 21:49:06 +0530

    Update and rename utils.py to script.py

---
+++ script.py
@@ -0,0 +1,5 @@
+class Script:
+    START_TXT = "👋 **Hello {mention}!**\nWelcome to **Raj HD Movies**.\nSearch for any movie to download instantly."
+    HELP_TXT = "**Using the Bot:**\n1. Type Movie Name.\n2. Click Button.\n3. File will auto-delete in 15 mins."
+    PREMIUM_TXT = "💎 **Premium Rates:**\n1 Month: ₹30\n1 Year: ₹250"
+    
