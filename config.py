889d5c14d0cc1e25ef3a63ff8207efcaa75de3fd
rajdev1009 <mrdev4772@gmail.com>
2025-12-14 21:44:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,30 @@
 import os
+from dotenv import load_dotenv
 
-API_ID = int(os.getenv("API_ID", "123456"))
-API_HASH = os.getenv("API_HASH", "your_api_hash")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
+load_dotenv()
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://...")
+class Config:
+    # API & Bot
+    API_ID = int(os.environ.get("API_ID", "0"))
+    API_HASH = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-# 👇👇 YE LINE MISSING THI
-ADMINS = list(map(int, os.getenv("ADMINS", "123456789").split()))
+    # Database
+    MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "")
+
+    # Channels (Integer Fix)
+    DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "0"))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "0"))
+    UPDATE_CHANNEL = int(os.environ.get("UPDATE_CHANNEL", "0"))
+
+    # Links
+    UPDATE_CHANNEL_LINK = "https://t.me/YOUR_UPDATE_LINK"
+    MOVIE_GROUP_LINK = "https://t.me/Raj_Hd_movies"
+    ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "RajHDAdmin") # Bina @ ke
+
+    # Admin ID
+    ADMINS = [int(x) for x in os.environ.get("ADMINS", "0").split()]
+
+    # Shortener (Optional)
+    SHORTENER_API = os.environ.get("SHORTENER_API", "")
+    SHORTENER_URL = os.environ.get("SHORTENER_URL", "") 
