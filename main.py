116d840f13c2bf9259080f96f40f72248d420848
rajdev1009 <mrdev4772@gmail.com>
2025-12-14 21:07:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
 
 app = Client(
-    "AutoFilterBot",
+    "auto-filter-bot",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
@@ -12,7 +12,7 @@ app = Client(
 
 async def main():
     await app.start()
-    print("✅ Bot Started Successfully")
+    print("✅ Bot Started")
     await asyncio.Event().wait()
 
 if __name__ == "__main__":
