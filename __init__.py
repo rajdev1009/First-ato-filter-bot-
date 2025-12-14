d18d516bc26359a27a3c221450ac506d300318c8
rajdev1009 <mrdev4772@gmail.com>
2025-12-14 20:06:42 +0530

    Delete __init__.py

--- __init__.py
+++
@@ -1,8 +0,0 @@
-# This file marks the plugins folder as a Python package.
-# Do not delete this file.
-plugins/
- ├── __init__.py   ✅ (must exist)
- ├── commands.py
- ├── filter.py
- ├── callbacks.py
- └── admin.py
